8ce4fd5ba7baa7c20484b97388f0319a344a0732
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-12-20 15:11:31 +0900

    feat: Theme 추가

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './components/providers/ThemeProvider';
 import MainPage from './pages/MainPage';
 import ArticlesPage from './pages/ArticlesPage';
 import LoginPage from './pages/LoginPage';
@@ -6,13 +7,15 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<MainPage />} />
-        <Route path="articles" element={<ArticlesPage />} />
-        <Route path="login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="articles" element={<ArticlesPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
